6273952aaf9154c516bc5c2b2b6ab18fec43aba2
Wen-Chao Huang <258099211@qq.com>
2019-04-07 01:47:33 +0800

    Instance

--- Projects/VisualVk/PhysicalDevice.cpp
+++ Projects/VisualVk/PhysicalDevice.cpp
@@ -2,6 +2,7 @@
 *********************    VisualVk_PhysicalDevice    **********************
 *************************************************************************/
 #include "PhysicalDevice.h"
+#include "LogicalDevice.h"
 
 using namespace Vk;
 
@@ -14,7 +15,133 @@ PhysicalDevice::PhysicalDevice(VkPhysicalDevice hDevice) : m_hPhysicalDevice(hDe
 }
 
 
+VkBool32 PhysicalDevice::IsSurfaceSupported(VkSurfaceKHR hSurface, uint32_t QueueFamilyIndex) const
+{
+	VkBool32 IsSupported = VK_FALSE;
+
+	if (QueueFamilyIndex >= m_QueueFamilyProperties.size())		return VK_FALSE;
+
+	vkGetPhysicalDeviceSurfaceSupportKHR(m_hPhysicalDevice, QueueFamilyIndex, hSurface, &IsSupported);
+
+	return IsSupported;
+}
+
+
+std::vector<VkPresentModeKHR> PhysicalDevice::GetSurfacePresentModes(VkSurfaceKHR hSurface) const
+{
+	uint32_t PresentModeCount = 0;
+
+	vkGetPhysicalDeviceSurfacePresentModesKHR(m_hPhysicalDevice, hSurface, &PresentModeCount, nullptr);
+
+	std::vector<VkPresentModeKHR> SurfacePresentModes(PresentModeCount);
+
+	vkGetPhysicalDeviceSurfacePresentModesKHR(m_hPhysicalDevice, hSurface, &PresentModeCount, SurfacePresentModes.data());
+
+	return SurfacePresentModes;
+}
+
+
+std::vector<VkSurfaceFormatKHR> PhysicalDevice::GetSurfaceFormats(VkSurfaceKHR hSurface) const
+{
+	uint32_t SurfaceFormatCount = 0;
+
+	vkGetPhysicalDeviceSurfaceFormatsKHR(m_hPhysicalDevice, hSurface, &SurfaceFormatCount, nullptr);
+
+	std::vector<VkSurfaceFormatKHR> SurfaceFormats(SurfaceFormatCount);
+
+	vkGetPhysicalDeviceSurfaceFormatsKHR(m_hPhysicalDevice, hSurface, &SurfaceFormatCount, SurfaceFormats.data());
+
+	return SurfaceFormats;
+}
+
+
+VkSurfaceCapabilitiesKHR PhysicalDevice::GetSurfaceCapabilities(VkSurfaceKHR hSurface) const
+{
+	VkSurfaceCapabilitiesKHR SurfaceCapabilities;
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(m_hPhysicalDevice, hSurface, &SurfaceCapabilities);
+
+	return SurfaceCapabilities;
+}
+
+
+VkFormatProperties PhysicalDevice::GetFormatProperties(VkFormat eFormat) const
+{
+	VkFormatProperties FormatProperties;
+
+	vkGetPhysicalDeviceFormatProperties(m_hPhysicalDevice, eFormat, &FormatProperties);
+
+	return FormatProperties;
+}
+
+
+const std::vector<VkQueueFamilyProperties> & PhysicalDevice::GetQueueFamilyProperties() const
+{
+	return m_QueueFamilyProperties;
+}
+
+
+const std::vector<VkExtensionProperties> & PhysicalDevice::GetExtensionProperties() const
+{
+	return m_ExtensionProperties;
+}
+
+
+const VkPhysicalDeviceMemoryProperties & PhysicalDevice::GetMemoryProperties() const
+{
+	return m_MemoryProperties;
+}
+
+
+const std::vector<VkLayerProperties> & PhysicalDevice::GetLayerProperties() const
+{
+	return m_LayerProperties;
+}
+
+
+const VkPhysicalDeviceProperties & PhysicalDevice::GetProperties() const
+{
+	return m_Properties;
+}
+
+
+const VkPhysicalDeviceFeatures & PhysicalDevice::GetFeatures() const
+{
+	return m_Features;
+}
+
+
+VkBool32 PhysicalDevice::IsExtensionAvailable(std::string TargetName) const
+{
+	for (size_t i = 0; i < m_ExtensionProperties.size(); i++)
+	{
+		if (TargetName == m_ExtensionProperties[i].extensionName)
+		{
+			return VK_TRUE;
+		}
+	}
+
+	return VK_FALSE;
+}
+
+
+VkResult PhysicalDevice::DestroyLogicalDevice(LogicalDevice * pLogicalDevice)
+{
+	if (m_pLogicalDevices.erase(pLogicalDevice) != 0)
+	{
+		delete pLogicalDevice;
+
+		return VK_SUCCESS;
+	}
+
+	return VK_ERROR_INVALID_EXTERNAL_HANDLE;
+}
+
+
 PhysicalDevice::~PhysicalDevice() noexcept
 {
-
+	for (auto pLogicalDevice : m_pLogicalDevices)
+	{
+		delete pLogicalDevice;
+	}
 }
--- Projects/VisualVk/PhysicalDevice.h
+++ Projects/VisualVk/PhysicalDevice.h
@@ -33,14 +33,52 @@ namespace Vk
 
 	public:
 
+		//!	@brief	If device extension is available.
+		VkBool32 IsExtensionAvailable(std::string TargetName) const;
 
+		//!	@brief	Destroy logical device.
+		VkResult DestroyLogicalDevice(LogicalDevice * pLogicalDevice);
 
+		//!	@brief	Return format properties.
+		VkFormatProperties GetFormatProperties(VkFormat eFormat) const;
+
+		//!	@brief	Return surface capabilities.
+		VkSurfaceCapabilitiesKHR GetSurfaceCapabilities(VkSurfaceKHR hSurface) const;
+
+		//!	@brief	Return surface formats.
+		std::vector<VkSurfaceFormatKHR> GetSurfaceFormats(VkSurfaceKHR hSurface) const;
+
+		//!	@brief	Return surface present modes.
+		std::vector<VkPresentModeKHR> GetSurfacePresentModes(VkSurfaceKHR hSurface) const;
+
+		//!	@brief	If present surface is supported by specify queue family.
+		VkBool32 IsSurfaceSupported(VkSurfaceKHR hSurface, uint32_t QueueFamilyIndex) const;
+
+		//!	@brief	Return queue family properties.
+		const std::vector<VkQueueFamilyProperties> & GetQueueFamilyProperties() const;
+
+		//!	@brief	Return extension properties.
+		const std::vector<VkExtensionProperties> & GetExtensionProperties() const;
+
+		//!	@brief	Return memory properties.
+		const VkPhysicalDeviceMemoryProperties & GetMemoryProperties() const;
+
+		//!	@brief	Return layer properties.
+		const std::vector<VkLayerProperties> & GetLayerProperties() const;
+
+		//!	@brief	Return physical properties.
+		const VkPhysicalDeviceProperties & GetProperties()const;
+
+		//!	@brief	Return physical features.
+		const VkPhysicalDeviceFeatures & GetFeatures() const;
 
 	private:
 
 		const VkPhysicalDevice					m_hPhysicalDevice;
 
 		std::set<LogicalDevice*>				m_pLogicalDevices;
+
+	private:
 
 		VkPhysicalDeviceFeatures				m_Features;
 
